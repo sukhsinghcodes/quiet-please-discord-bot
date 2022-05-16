9f0b2f317d59c666ee2aaa970e94615ca6785c0c
Sukhvinder Singh Durhailay <sukhvinder@durhailay.co>
2022-05-16 14:13:50 +0100

    added prettier

--- src/index.ts
+++ src/index.ts
@@ -9,31 +9,33 @@ dotenv.config();
 const messagesToListenFor = ['quiet', 'hey', 'mo', 'mo?'];
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES], partials: ['CHANNEL'] });
-console.log('Starting Quiet Please Bot...')
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES],
+  partials: ['CHANNEL'],
+});
+console.log('Starting Quiet Please Bot...');
 
 const giphyFetch = new GiphyFetch(process.env.GIPHY_API_KEY);
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
-	console.log('Ready!');
+  console.log('Ready!');
 });
 
-client.on('messageCreate', async message => {
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) {
+    return;
+  }
 
-	if (message.author.bot) {
-		return;
-	}
+  const words = message.content.split(' ');
 
-	const words = message.content.split(' ');
-
-	for (const word of words) {
-		if (messagesToListenFor.some(msg => msg === word.toLowerCase())) {
-			const { data: gif } = await giphyFetch.random({ tag: 'quiet please' });
-			message.reply(`Quiet plz!\n${gif.images.original.url}`);
-			return;
-		}
-	}
+  for (const word of words) {
+    if (messagesToListenFor.some((msg) => msg === word.toLowerCase())) {
+      const { data: gif } = await giphyFetch.random({ tag: 'quiet please' });
+      message.reply(`Quiet plz!\n${gif.images.original.url}`);
+      return;
+    }
+  }
 });
 
 // Login to Discord with your client's token
