ab88a70e225c0f0d796d12ec2cc47d3a02dd3582
Sukhvinder Singh Durhailay <sukhvinder@durhailay.co>
2022-05-24 14:39:25 +0100

    refactored MessageCreate event into a class

--- src/events/onMessageCreate.ts
+++ src/events/onMessageCreate.ts
@@ -1,27 +1,31 @@
 import { Message } from 'discord.js';
-import { GiphyFetch } from '@giphy/js-fetch-api';
 
-const giphyFetch = new GiphyFetch(process.env.GIPHY_API_KEY);
+export class MessageCreate {
+  private quietWords = ['quiet', 'quietings', 'quieting'];
+  private moWords = ['mo', 'mo?', 'mouhannad', 'mouhannad!', 'mo!'];
+  private giphyFetch;
 
-const quietWords = ['quiet', 'quietings', 'quieting'];
-const moWords = ['mo', 'mo?', 'mouhannad', 'mouhannad!', 'mo!'];
-
-export async function onMessageCreate(message: Message<boolean>): Promise<void> {
-  if (message.author.bot) {
-    return;
+  constructor(giphyFetch) {
+    this.giphyFetch = giphyFetch;
   }
 
-  const words = message.content.split(' ');
+  public onMessageCreate = async (message: Message<boolean>): Promise<void> => {
+    if (message.author.bot) {
+      return;
+    }
 
-  for (const word of words) {
-    if (moWords.some((msg) => msg === word.toLowerCase())) {
-      message.channel.send('Mo?');
-      return;
+    const words = message.content.split(' ');
+
+    for (const word of words) {
+      if (this.moWords.some((msg) => msg === word.toLowerCase())) {
+        message.channel.send('Mo?');
+        return;
+      }
+      if (this.quietWords.some((msg) => msg === word.toLowerCase())) {
+        const { data: gif } = await this.giphyFetch.random({ tag: 'quiet please' });
+        message.channel.send(`Quiet please!\n${gif.images.original.url}`);
+        return;
+      }
     }
-    if (quietWords.some((msg) => msg === word.toLowerCase())) {
-      const { data: gif } = await giphyFetch.random({ tag: 'quiet please' });
-      message.channel.send(`Quiet please!\n${gif.images.original.url}`);
-      return;
-    }
-  }
+  };
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 // Require the necessary discord.js classes
 import { Client, Intents } from 'discord.js';
 import dotenv from 'dotenv';
-import { onMessageCreate } from './events';
+import { GiphyFetch } from '@giphy/js-fetch-api';
+import { MessageCreate } from './events';
 
 dotenv.config();
 
@@ -11,12 +12,16 @@ const client = new Client({
   partials: ['CHANNEL'],
 });
 
+const giphyFetch = new GiphyFetch(process.env.GIPHY_API_KEY);
+
+const messageCreate = new MessageCreate(giphyFetch);
+
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
   console.log('Quiet Please bot online!');
 });
 
-client.on('messageCreate', onMessageCreate);
+client.on('messageCreate', messageCreate.onMessageCreate);
 
 // Login to Discord with your client's token
 client.login(process.env.DISCORD_TOKEN);
