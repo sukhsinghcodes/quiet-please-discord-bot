4efa77a507756f233590f9001e7ece2276ac3b91
Sukhvinder Singh Durhailay <sukhvinder@durhailay.co>
2022-06-01 01:03:35 +0100

    downgraded node version and added fetch polyfill

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,24 @@ import { Client, Intents } from 'discord.js';
 import dotenv from 'dotenv';
 import { GiphyFetch } from '@giphy/js-fetch-api';
 import { MessageCreate } from './events';
+import fetch, { Headers, Request, Response } from 'node-fetch';
+
+if (!global.fetch) {
+  const agent = ({ protocol }) =>
+    protocol === 'http:' ? global.__NEXT_HTTP_AGENT : global.__NEXT_HTTPS_AGENT;
+  const fetchWithAgent = (url, opts) => {
+    if (!opts) {
+      opts = { agent };
+    } else if (!opts.agent) {
+      opts.agent = agent;
+    }
+    return fetch(url, opts);
+  };
+  global.fetch = fetchWithAgent;
+  global.Headers = Headers;
+  global.Request = Request;
+  global.Response = Response;
+}
 
 dotenv.config();
 
